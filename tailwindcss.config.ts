b096b2c1a0cdcf4b8addd8859c0a2c170faeda65
yi-ting <yumy19990628@gmail.com>
2025-11-23 22:57:49 +0800

    feat: 新增定義fonts style, colors

--- tailwindcss.config.ts
+++ tailwindcss.config.ts
@@ -1,3 +1,18 @@
 import type { Config } from 'tailwindcss';
 
-export default <Partial<Config>>{};
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        'blue-01': '#D6F2FA',
+        'blue-02': '#9FD8F0',
+        'blue-03': '#0095CC',
+        'red-01': '#FFEDED',
+        'red-02': '#FFA59B',
+        'red-03': '#FE7152',
+        'dark': '#4C4C4C',
+        'text': '#404040',
+      },
+    },
+  },
+};
