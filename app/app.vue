a474f1c754da8982850adfe784cd33f9192a96e3
yi-ting <yumy19990628@gmail.com>
2025-11-26 00:30:09 +0800

    feat: sectionC layout 缺功能

--- app/app.vue
+++ app/app.vue
@@ -1,38 +1,38 @@
 <script lang="ts" setup>
-import AppHeader from './components/AppHeader.vue';
-import AppFooter from './components/AppFooter.vue';
-import LSectionA from './components/LSectionA.vue';
-import LSectionB from './components/LSectionB.vue';
-import LSectionC from './components/LSectionC.vue';
-import LSectionD from './components/LSectionD.vue';
-import LSectionE from './components/LSectionE.vue';
-import LSectionF from './components/LSectionF.vue';
-import TestAllImg from './components/TestAllImg.vue';
-import meta from './locales/meta.json';
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+import LSectionA from "./components/LSectionA.vue";
+import LSectionB from "./components/LSectionB.vue";
+import LSectionC from "./components/LSectionC.vue";
+// import LSectionD from "./components/LSectionD.vue";
+// import LSectionE from "./components/LSectionE.vue";
+// import LSectionF from "./components/LSectionF.vue";
+import TestAllImg from "./components/TestAllImg.vue";
+import meta from "./locales/meta.json";
 
 useSeoMeta({
-  title: meta.metaTitle,
-  description: meta.metaDesc,
+	title: meta.metaTitle,
+	description: meta.metaDesc,
 });
 
 useJsonld({
-  '@context': 'https://schema.org',
-  '@type': 'WebPage',
-  name: meta.metaTitle,
-  description: meta.metaDesc,
+	"@context": "https://schema.org",
+	"@type": "WebPage",
+	name: meta.metaTitle,
+	description: meta.metaDesc,
 });
 </script>
 
 <template>
-  <NuxtLayout>
-    <AppHeader />
-    <LSectionA />
-    <LSectionB />
-    <LSectionC />
-    <LSectionD />
+	<NuxtLayout>
+		<AppHeader />
+		<LSectionA />
+		<LSectionB />
+		<LSectionC />
+		<!-- <LSectionD />
     <LSectionE />
-    <LSectionF />
-    <AppFooter />
-    <TestAllImg />
-  </NuxtLayout>
+    <LSectionF /> -->
+		<AppFooter />
+		<TestAllImg />
+	</NuxtLayout>
 </template>
