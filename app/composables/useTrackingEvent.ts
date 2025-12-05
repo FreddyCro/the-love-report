38b7cf3665d7886485b2c4b0cc6e093e1b5b981b
Freddy Yang <freddy10100@hotmail.com>
2025-12-05 08:06:11 +0800

    feat: update GA tracking functions to use 'term' category and improve labeling

--- app/composables/useTrackingEvent.ts
+++ app/composables/useTrackingEvent.ts
@@ -1,7 +1,40 @@
-function gaLandingSectionView(label: string) {
+// 元素可見度
+// Label: keyvisual-upper ,keyvisual-lower ,overview ,story ,AI_CH1 ,AI_CH2 ,AI_CH3 ,AI_CH4 ,value ,trust ,busy ,3C ,unrealistic ,question ,editor
+function gaSectionView(label: string) {
   sendGA({
     action: 'section_view',
-    category: 'landing',
+    category: 'term',
+    label,
+  });
+}
+
+// 個案故事－錨點按鈕
+// Label: 小莉 ,保羅 ,小B ,Michael ,彥哥 ,紋子
+function gaClickAnchor(label: string) {
+  sendGA({
+    action: 'click_anchor',
+    category: 'term',
+    label,
+  });
+}
+
+// 個案故事－展開按鈕: story_小莉打開 ,story_保羅打開 ,story_小B打開 ,story_Michael打開 ,story_彥哥打開 ,story_紋子打開
+function gaClickSeries(label: string) {
+  sendGA({
+    action: 'click_series',
+    category: 'term',
+    label,
+  });
+}
+
+// 個案故事－錨點切換按鈕: left_左側按鈕, right_右側按鈕
+// 個案故事－關閉按鈕: story_小莉關閉 ,story_保羅關閉 ,story_小B關閉 ,story_Michael關閉 ,story_彥哥關閉 ,story_紋子關閉
+// 常見問題－展開按鈕: question_Q1打開, question_Q2打開, question_Q3打開, question_Q4打開, question_Q5打開, question_Q6打開
+// 常見問題－關閉按鈕: question_Q1關閉, question_Q2關閉, question_Q3關閉, question_Q4關閉, question_Q5關閉, question_Q6關閉
+function gaClickBtn(label: string) {
+  sendGA({
+    action: 'click_btn',
+    category: 'term',
     label,
   });
 }
@@ -50,6 +83,9 @@ function sendGA({ category, action, label }: GAEvent) {
 export default () => {
   return {
     sendGA,
-    gaLandingSectionView,
+    gaSectionView,
+    gaClickAnchor,
+    gaClickSeries,
+    gaClickBtn,
   };
 };
